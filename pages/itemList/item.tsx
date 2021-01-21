210c9ae76535ec90a5b2aa3040554091102801a9
alexis <alexisbrou1@hotmail.fr>
2021-01-21 09:31:05 -0500

    adds color depending on user

--- pages/itemList/item.tsx
+++ pages/itemList/item.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import {ListItem, ListItemText, ListItemSecondaryAction, Checkbox, IconButton} from '@material-ui/core';
+import {ListItem, ListItemText, ListItemSecondaryAction, Checkbox, IconButton, ListItemAvatar, Avatar} from '@material-ui/core';
 import {T_ItemProps} from '../../objectTypes/item';
 import {Delete} from '@material-ui/icons';
 const Item = (props: T_ItemProps) => {
-    const labelId = "checkbox-list-secondary-label-" + props.index;
     return (
         <ListItem key={props.index} button>
-            <ListItemText id={labelId} primary={props.item.name} secondary={props.item.total} />
+            <ListItemAvatar>
+              <Avatar
+                alt={`Avatar color`}
+                src={`/img/${props.item.user.color}.jpg`}
+              />
+            </ListItemAvatar>
+            <ListItemText primary={props.item.name} secondary={props.item.total} />
             <ListItemSecondaryAction>
                 <IconButton aria-label="delete" onClick={()=>props.delete(props.index)}>
                     <Delete fontSize="small" />
